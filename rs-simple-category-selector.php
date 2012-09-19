ad8a4938e052d8d4073e60255bd9891e058edb51
robsaurini <robsaurini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-19 23:26:10 +0000

    fixed issue with WP_PLUGIN_URL giving the wrong scheme in the url when the js and css are included
    git-svn-id: https://plugins.svn.wordpress.org/rs-simple-category-selector/trunk@601347 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rs-simple-category-selector.php
+++ rs-simple-category-selector.php
@@ -32,7 +32,7 @@ if ( ! defined( 'RSSCS_PLUGIN_NAME' ) )
 	define( 'RSSCS_PLUGIN_NAME', trim( dirname( RSSCS_PLUGIN_BASENAME ), '/' ) );
 	
 if ( ! defined( 'RSSCS_PLUGIN_URL' ) )
-	define( 'RSSCS_PLUGIN_URL', WP_PLUGIN_URL . '/' . RSSCS_PLUGIN_NAME );
+	define( 'RSSCS_PLUGIN_URL', plugins_url()  . '/' . RSSCS_PLUGIN_NAME );
 
 class RS_Simple_Category_Selector {
 
@@ -73,7 +73,7 @@ class RS_Simple_Category_Selector {
 		if ( empty( $_GET['postid'] ) )
 			die( 'No post has been selected' );
 
-		$post_id = (int) $_GET['postid'];
+		$post_id = $_GET['postid'];
 
 		if ( ! is_admin() && current_user_can( 'edit_post', $post_id ) )
 			die( 'Not in the admin' );
